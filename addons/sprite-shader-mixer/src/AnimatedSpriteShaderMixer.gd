85727425c30d1b72a70da8a8c890bf0f03d9d177
spheras <joseamuedo@gmail.com>
2023-09-25 20:10:27 +0200

    super godot

--- addons/sprite-shader-mixer/src/AnimatedSpriteShaderMixer.gd
+++ addons/sprite-shader-mixer/src/AnimatedSpriteShaderMixer.gd
@@ -3,20 +3,26 @@
 extends AnimatedSprite2D
 class_name AnimatedSprite2DShaderMixer
 
-const SHADER_FOLDER_BASE = "res://addons/godot_sprite_shader/assets/shader/"
 const mainShader = preload("res://addons/sprite-shader-mixer/assets/shader.gdshader")
-
 @export_group("Shader Mixer")
-@export var shaderMixer:ShaderMixer
-	
-func _ready():
+@export var shaderMixer:ShaderMixer: set = _onSetShaderMixer
+
+func _onSetShaderMixer(_shaderMixer):
+	shaderMixer=_shaderMixer
+	shaderMixer.setParentSprite(self)
+	_checkShaderMixer()
+
+func _checkShaderMixer():
 	if(self.shaderMixer == null):
-		print_debug("creamos un shaderMixer")
 		var newShaderMixer=ShaderMixer.new()
 		shaderMixer=newShaderMixer
 
 	if(self.material == null):
-		print_debug("creamos un shaderMaterial")
 		var shaderMaterial = ShaderMaterial.new()
 		shaderMaterial.shader=self.mainShader;
 		self.material=shaderMaterial
+
+	self.shaderMixer.setParentSprite(self)
+	
+func _ready():
+	_checkShaderMixer()
--- addons/sprite-shader-mixer/src/SpriteShaderMixer.gd
+++ addons/sprite-shader-mixer/src/SpriteShaderMixer.gd
@@ -3,26 +3,27 @@
 extends Sprite2D
 class_name Sprite2DShaderMixer
 
-const SHADER_FOLDER_BASE = "res://addons/godot_sprite_shader/assets/shader/"
 const mainShader = preload("res://addons/sprite-shader-mixer/assets/shader.gdshader")
-
 @export_group("Shader Mixer")
 @export var shaderMixer:ShaderMixer: set = _onSetShaderMixer
 
 func _onSetShaderMixer(_shaderMixer):
+	print_debug("setting shader mixer!!!")
 	shaderMixer=_shaderMixer
 	shaderMixer.setParentSprite(self)
+	_checkShaderMixer()
 
-func _ready():
-	print_debug("ready")
-
+func _checkShaderMixer():
 	if(self.shaderMixer == null):
 		var newShaderMixer=ShaderMixer.new()
 		shaderMixer=newShaderMixer
 
 	if(self.material == null):
 		var shaderMaterial = ShaderMaterial.new()
-		shaderMaterial.shader=self.mainShader;
+		shaderMaterial.shader=self.mainShader
 		self.material=shaderMaterial
 
 	self.shaderMixer.setParentSprite(self)
+	
+func _ready():
+	_checkShaderMixer()
